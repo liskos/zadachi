a68a79b2cf1f30e913034b3e7f2dc494a3703d78
liskos <liskosnik2011@yandex.ru>
2020-07-31 23:44:49 +0300

    initiation и 1 задание

---
+++ 1.py
@@ -0,0 +1,12 @@
+N = 30
+x1 = int(input())
+x2 = int(input())
+maximum = x1 + x2
+number = 1
+for i in range(2, N):
+    x1 = x2
+    x2 = int(input())
+    if x1 + x2 > maximum:
+        maximum = x1 + x2
+        number = i
+print(number)
