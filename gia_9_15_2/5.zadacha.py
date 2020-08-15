126459c64269030fbd1489f04e045f1626fe7261
Никита <nikita.iwanow.222@gmail.com>
2020-08-15 18:56:45 +0300

    5-ая задача

---
+++ gia_9_15_2/5.zadacha.py
@@ -0,0 +1,15 @@
+# 26-ая задача
+
+import sys as s
+
+s.stdin = open(file='in/5.in', mode='r', encoding='utf-8')
+# s.stdout = open(file='in/5.out', mode='w', encoding='utf-8')
+
+chislo = 1
+colichestvo = 0
+while chislo != 0:
+    chislo = int(input())
+    if chislo % 6 == 0 and chislo % 10 == 4:
+        colichestvo += 1
+
+print(colichestvo)
