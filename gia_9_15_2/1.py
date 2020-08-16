05b76f803f13750871f00733cf80509ffea8a554
Никита <nikita.iwanow.222@gmail.com>
2020-08-16 17:14:07 +0300

    10-ый номер

--- gia_9_15_2/1.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-sys.stdin = open(file="in/25.in", mode="r", encoding="utf-8")
-
-a = int(input())
-b = int(input())
-
-print((b - a + 1) // 2) if (b - a + 1) % 2 == 0 else print(((b - a + 1) + 1) // 2)
