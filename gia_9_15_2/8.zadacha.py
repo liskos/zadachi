057aeb15f36b3a3ff3b28cf722aba6eceff7d5e8
Никита <nikita.iwanow.222@gmail.com>
2020-08-16 16:38:50 +0300

    Исправил 8-ой номер

--- gia_9_15_2/8.zadacha.py
+++ gia_9_15_2/8.zadacha.py
@@ -4,10 +4,9 @@ import sys as s
 
 s.stdin = open(file='in/8.in', mode='r', encoding='utf-8')
 s.stdout = open(file='out/8.out', mode='w', encoding='utf-8')
-
-chislo = 1
 col_chisel = 0
-while chislo != 0:
+сol_chift = int(input())
+for _ in range(сol_chift):
     chislo = int(input())
     if chislo % 6 == 0 and chislo % 10 == 4:
         col_chisel += 1
