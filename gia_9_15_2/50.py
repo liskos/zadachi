6231029427604b8bcce4ca2cd46a40e92e138a15
Никита <nikita.iwanow.222@gmail.com>
2020-08-16 16:34:55 +0300

    Исправил 50-ую задачу

--- gia_9_15_2/50.py
+++ gia_9_15_2/50.py
@@ -6,15 +6,14 @@ s.stout = open(file='out/50.out', mode='w', encoding='utf-8')
 
 shesterki = []
 sravn = [1]
-chislo = 1
-while chislo != 0:
+col_chifr = int(input())
+for _ in range(col_chifr):
     chislo = int(input())
     if chislo % 10 == 6:
         shesterki.append(chislo)
-if shesterki > sravn:
-    minimum_chislo = min(shesterki)
-    print(minimum_chislo)
+if sravn > shesterki:
+    "Таких цифр нет"
 else:
-    print("Таких числ нет")
-
+    Minimum_6 = min(shesterki)
+    print(Minimum_6)
 #  неверно, делается через цикл for, нет в конце нуля,  и без функции min
